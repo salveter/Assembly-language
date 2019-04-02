f372fa72a7f2a33fbeb4f7290648cba4af46bcb7
eagles <me@me.com>
2019-04-02 20:53:44 +0800

    增加了ASCII函数

--- function.asm
+++ function.asm
@@ -1,6 +1,6 @@
 ;--------------------------将es:bx开始的字符输出，直到遇见CL的内容
-display:
-       back:
+DISPLAY PROC
+       BACK:
             CMP BYTE PTR ES:[BX],CL
             JZ DONE
             MOV DL,ES:[BX]
@@ -10,3 +10,31 @@ display:
             JMP SHORT BACK
        DONE:
             RET 
+DISPLAY ENDP
+
+;--------------------------------------------------------
+
+;------------------将AL中的内容转换为ASCII码
+
+TO_ASCII PROC
+         AND AL,0FH
+         CMP AL,10
+         JC NUM;说明AL后半部分小于等于'9',就直接加上'0'的ASCII码即可
+         ADD AL,07H;说明AL后半部分为A-H，要在‘0’的基础上加上7
+     NUM:ADD AL,30H
+         RET
+TO_ASCII ENDP
+
+ASCII PROC
+      PUSH CX
+      MOV CH,AL
+      MOV CL,4
+      SHR AL,CL
+      CALL TO_ASCII;处理AL的高四位
+      MOV AH,AL
+      MOV AL,CH
+      CALL TO_ASCII
+      POP CX
+      RET
+ASCII ENDP                
+;-----------------------------------------------------------
